978253b626ad409656d626746f9e291bef724730
Sarvesh Tiku <sarveshtiku@gmail.com>
2025-06-17 20:08:41 -0700

    Edited package.json to include vite preview to delpoy

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    host: true, // already set by --host
+    // allow your Render domain (and any subdomain of onrender.com if you like)
+    allowedHosts: [
+      'localhost',
+      'flixster-project-unit-3.onrender.com'
+    ]
+  }
 })
